6d5cb29b928796e37c520f05097077bd28b1949d
minicast <stefan.minica@gmail.com>
2021-03-14 10:25:51 +0100

    add random doc query

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,37 @@
 import os
+import time
 import streamlit as st
 from dotenv import load_dotenv
+from elasticsearch import Elasticsearch, helpers
 
 load_dotenv()
 
-LOCAL_URL = 'localhost:9200'; 
-BONSAI_URL = os.getenv('BONSAI_URL'); SEARCHLY_URL = os.getenv('SEARCHLY_URL'); 
-AWS_ES_URL = os.getenv('AWS_ES_URL'); CLOUD_ES_URL = os.getenv('CLOUD_ES_URL')
+LOCAL_URL = 'localhost:9200'
+BONSAI_URL = os.getenv('BONSAI_URL')
+SEARCHLY_URL = os.getenv('SEARCHLY_URL')
+AWS_ES_URL = os.getenv('AWS_ES_URL')
+CLOUD_ES_URL = os.getenv('CLOUD_ES_URL')
 ES_URL_LABELS = ['Local Elasticsearch URL', 'Bonsai Elasticsearch URL', 'Searchly Elasticsearch URL',
-    'AWS Elasticsearch URL', 'Cloud Elasticsearch URL' ]
-ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL, SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
+                 'AWS Elasticsearch URL', 'Cloud Elasticsearch URL']
+ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL,
+                  SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
 
 if st.checkbox(f'Configure Elasticsearch URLs (default value: {LOCAL_URL})'):
-    LOCAL_URL = st.text_input('Local Elasticsearch URL', LOCAL_URL, max_chars=500, key='es_local_url')
-    BONSAI_URL = st.text_input('Bonsai Elasticsearch URL', max_chars=500, key='es_bonsai_url')
-    SEARCHLY_URL = st.text_input('Searchly Elasticsearch URL', max_chars=500, key='es_searchly_url')
-    AWS_ES_URL = st.text_input('AWS Elasticsearch URL', max_chars=500, key='es_aws_url')
-    CLOUD_ES_URL = st.text_input('Cloud Elasticsearch URL', max_chars=500, key='es_cloud_url')
-    ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL, SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
+    LOCAL_URL = st.text_input(
+        'Local Elasticsearch URL', LOCAL_URL, max_chars=500, key='es_local_url')
+    BONSAI_URL = st.text_input(
+        'Bonsai Elasticsearch URL', max_chars=500, key='es_bonsai_url')
+    SEARCHLY_URL = st.text_input(
+        'Searchly Elasticsearch URL', max_chars=500, key='es_searchly_url')
+    AWS_ES_URL = st.text_input(
+        'AWS Elasticsearch URL', max_chars=500, key='es_aws_url')
+    CLOUD_ES_URL = st.text_input(
+        'Cloud Elasticsearch URL', max_chars=500, key='es_cloud_url')
+    ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL,
+                      SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
 
-ACTIVE_URL_LABEL = st.selectbox('Select the active Elasticsearch URL: ', ES_URL_LABELS, key='box_select_url')
+ACTIVE_URL_LABEL = st.selectbox(
+    'Select the active Elasticsearch URL: ', ES_URL_LABELS, key='box_select_url')
 
 ES_URLS_DICT = dict(list(zip(ES_URL_LABELS, ES_URL_STRINGS)))
 ACTIVE_URL = ES_URLS_DICT[ACTIVE_URL_LABEL]
@@ -29,9 +41,17 @@ st.write(f'Your acive elastcsearch URL is: {ACTIVE_URL_LABEL}')
 if not ACTIVE_URL:
     st.write(f'Your acive elastcsearch URL is empty, please configure it above')
 else:
-    if ACTIVE_URL_LABEL == ES_URL_LABELS[0]: # localhost URL can be fully shown in the open 
+    # localhost URL can be fully shown in the open
+    if ACTIVE_URL_LABEL == ES_URL_LABELS[0]:
         st.write(f'Your acive elastcsearch URL is: {ACTIVE_URL}')
     else:                                    # obfuscate sensitive entries in the URL string
-        st.write(f'Your acive elastcsearch URL is: https://<YOUR_USER>:********@{ACTIVE_URL.split("@")[1]}')
+        st.write(
+            f'Your acive elastcsearch URL is: https://<YOUR_USER>:********@{ACTIVE_URL.split("@")[1]}')
 
+client = Elasticsearch(ACTIVE_URL)
+query_body = {"size": 1, "query": {
+    "function_score": {"functions": [{"random_score": {
+        "seed": int(time.time())}}]}}}
+result = client.search(index="document", body=query_body)
 
+st.write(result['hits']['hits'][0]['_source']['text'])
