ae3011a2fed99943cc86e07eae34acd585e046a1
Stefan minica <stefan.minica@gmail.com>
2021-04-15 15:45:40 +0200

    basic streamlit secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import os
 import time
+import json
 import streamlit as st
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch, helpers
+from expertai.nlapi.cloud.client import ExpertAiClient
 
 load_dotenv()
 
@@ -11,47 +13,47 @@ BONSAI_URL = os.getenv('BONSAI_URL')
 SEARCHLY_URL = os.getenv('SEARCHLY_URL')
 AWS_ES_URL = os.getenv('AWS_ES_URL')
 CLOUD_ES_URL = os.getenv('CLOUD_ES_URL')
+os.environ["EAI_USERNAME"] = os.getenv('EAI_USERNAME')
+os.environ["EAI_PASSWORD"] = os.getenv('EAI_PASSWORD')
+
 ES_URL_LABELS = ['Local Elasticsearch URL', 'Bonsai Elasticsearch URL', 'Searchly Elasticsearch URL',
                  'AWS Elasticsearch URL', 'Cloud Elasticsearch URL']
 ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL,
                   SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
 
-if st.checkbox(f'Configure Elasticsearch URLs (default value: {LOCAL_URL})'):
-    LOCAL_URL = st.text_input(
-        'Local Elasticsearch URL', LOCAL_URL, max_chars=500, key='es_local_url')
-    BONSAI_URL = st.text_input(
-        'Bonsai Elasticsearch URL', max_chars=500, key='es_bonsai_url')
-    SEARCHLY_URL = st.text_input(
-        'Searchly Elasticsearch URL', max_chars=500, key='es_searchly_url')
-    AWS_ES_URL = st.text_input(
-        'AWS Elasticsearch URL', max_chars=500, key='es_aws_url')
-    CLOUD_ES_URL = st.text_input(
-        'Cloud Elasticsearch URL', max_chars=500, key='es_cloud_url')
-    ES_URL_STRINGS = [LOCAL_URL, BONSAI_URL,
-                      SEARCHLY_URL, AWS_ES_URL, CLOUD_ES_URL]
-
-ACTIVE_URL_LABEL = st.selectbox(
-    'Select the active Elasticsearch URL: ', ES_URL_LABELS, key='box_select_url')
+ACTIVE_URL_LABEL = 'Searchly Elasticsearch URL'
 
 ES_URLS_DICT = dict(list(zip(ES_URL_LABELS, ES_URL_STRINGS)))
+ES_INDEX_KEYS = ['document', 'document', 'asoiaf', '', '']
+ES_INDEXES_DICT = dict(list(zip(ES_URL_LABELS, ES_INDEX_KEYS)))
+ES_INDEX_FIELD = ['text', 'text', 'paragraph_text', '', '']
+ES_FIELDS_DICT = dict(list(zip(ES_URL_LABELS, ES_INDEX_FIELD)))
 ACTIVE_URL = ES_URLS_DICT[ACTIVE_URL_LABEL]
+ACTIVE_INDEX = ES_INDEXES_DICT[ACTIVE_URL_LABEL]
+ACTIVE_FIELD = ES_FIELDS_DICT[ACTIVE_URL_LABEL]
 
-st.write(f'Your acive elastcsearch URL is: {ACTIVE_URL_LABEL}')
-
-if not ACTIVE_URL:
-    st.write(f'Your acive elastcsearch URL is empty, please configure it above')
-else:
-    # localhost URL can be fully shown in the open
-    if ACTIVE_URL_LABEL == ES_URL_LABELS[0]:
-        st.write(f'Your acive elastcsearch URL is: {ACTIVE_URL}')
-    else:                                    # obfuscate sensitive entries in the URL string
-        st.write(
-            f'Your acive elastcsearch URL is: https://<YOUR_USER>:********@{ACTIVE_URL.split("@")[1]}')
-
-client = Elasticsearch(ACTIVE_URL)
+client = Elasticsearch(SEARCHLY_URL)
 query_body = {"size": 1, "query": {
     "function_score": {"functions": [{"random_score": {
         "seed": int(time.time())}}]}}}
-result = client.search(index="document", body=query_body)
+result = client.search(index=ACTIVE_INDEX, body=query_body)
 
-st.write(result['hits']['hits'][0]['_source']['text'])
+expert_ai_client = ExpertAiClient()
+
+passage_text = result['hits']['hits'][0]['_source'][ACTIVE_FIELD]
+language= 'en'
+
+output = expert_ai_client.specific_resource_analysis(body={"document": {"text": passage_text}}, 
+    params={'language': language, 'resource': 'entities'})
+
+entities_from = []
+for entity in output.entities:
+    entities_from.append(str(entity.lemma))
+
+blurred = passage_text
+for entity in entities_from:
+    blurred = blurred.replace(entity, '[XXXX]')
+
+st.write(blurred)
+
+
